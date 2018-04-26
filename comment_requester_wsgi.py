7f07b44254f984f3926c9ea758c705ef5fdca8c1
Jeff Kaufman <jeff@jefftk.com>
2018-04-26 13:21:38 +0000

    force ipv4

--- comment_requester_wsgi.py
+++ comment_requester_wsgi.py
@@ -15,9 +15,19 @@ import subprocess
 import base64
 from xml.dom import minidom
 from collections import defaultdict
+import socket
 
 sys.path.append(os.path.dirname(__file__))
 from private import *
+
+# Monkey patch to force IPv4, since FB seems to hang on IPv6
+old_getaddrinfo = socket.getaddrinfo
+def new_getaddrinfo(*args, **kwargs):
+    responses = old_getaddrinfo(*args, **kwargs)
+    return [response
+            for response in responses
+            if response[0] == socket.AF_INET]
+socket.getaddrinfo = new_getaddrinfo
 
 def unescape(s):
     return (s.replace('&lt;', '<')
