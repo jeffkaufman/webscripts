f7ef193ac8d1b80d413bfd213e4908c6c737e23a
Jeff Kaufman <jeff.t.kaufman@gmail.com>
2019-06-19 11:49:46 +0000

    keep wide pre blocks from making the page horizontally scrollable on mobile

--- makerss.py
+++ makerss.py
@@ -649,6 +649,9 @@ body {margin: 0}
   text-align: right;
   margin-right: 0.5rem;
 }
+pre {
+  overflow-x: hidden;
+}
 ''',
 
   'amp_boilerplate': '<style amp-boilerplate="">body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style>',
