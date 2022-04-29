2dacfd9353998a1851d06c77892f46059eff8dd2
Jeff Kaufman <jeff.t.kaufman@gmail.com>
2022-04-29 10:16:05 -0400

    version control makerss-and-reverserss.sh

---
+++ makerss-and-reverserss.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+RSS_SOURCES="
+  -s https://apenwarr.ca/log/rss.php
+  -s https://www.benkuhn.net/index.xml
+  -s https://danluu.com/post/atom.xml
+  -s https://juliawise.net/feed/
+  -s https://sideways-view.com/feed/
+  -s https://www.lilywise.com/posts.rss
+  -s https://www.annawise.net/posts.rss
+  -s https://cullenokeefe.com/blog?format=rss"
+
+/home/jefftk/code/openring/openring \
+  $RSS_SOURCES \
+  < /home/jefftk/code/openring/in-small.html \
+  | sed 's~https://danluu.com/post/atom/index.xml~https://danluu.com~g' \
+  > /home/jefftk/jtk/current-openring.html && \
+/home/jefftk/code/openring/openring \
+  -n 100 -p 5 -l 500 $RSS_SOURCES \
+  < /home/jefftk/code/openring/in-big.html \
+  | sed 's~https://danluu.com/post/atom/index.xml~https://danluu.com~g' \
+  > /home/jefftk/jtk/ring.html && echo "feeds fetched" && \
+/home/jefftk/bin/makerss.py && echo "rss made" && \
+/home/jefftk/bin/reverserss.py && echo "done"
