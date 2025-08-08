b7d6b756b634a9d7ef4156ba304b16f3098abe15
Jeff Kaufman <jeff.t.kaufman@gmail.com>
2025-08-08 16:18:16 +0000

    Add Evan's blog to blogroll

--- makerss-and-reverserss.sh
+++ makerss-and-reverserss.sh
@@ -11,6 +11,7 @@ RSS_SOURCES="
   -s https://vkrakovna.wordpress.com/feed/
   -s https://chromamine.com/feeds/posts.xml
   -s https://blog.rossry.net/rss/
+  -s https://evanfields.net/feed.xml
   -s https://www.cold-takes.com/rss/"
 
 /home/jefftk/code/webscripts/makerss.py && echo "rss made" && \
